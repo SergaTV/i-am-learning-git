03e8294087280655447e43e6f4b9c8a140452406
SergaTV <boscn@yandex.ru>
2022-02-22 21:23:09 +0300

    `Hello, World` added.

---
+++ Programming_Stuff/hello.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+// if you see this then i have some questions for you.
+int main() {
+    cout << "My first \"Hello, World!\" in C++. Proud of it! =)" << endl;
+    return 0;
+}
